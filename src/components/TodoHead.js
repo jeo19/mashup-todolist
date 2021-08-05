ab940afe6033b7bf42c950c81f2bfa882d7d8eab
jeo19 <terrakkk@outlook.com>
2021-08-05 11:25:10 +0800

    feat:Add the TodoHead styled-components

---
+++ src/components/TodoHead.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+
+const TodoHeadBlock = styled.div`
+  padding-top: 48px;
+  padding-left: 32px;
+  padding-right: 32px;
+  padding-bottom: 24x;
+  border-bottom: 1px solid #e9ecef;
+
+  h1 {
+    margin: 0;
+    font-size: 36px;
+    color: #343a40;
+  }
+
+  .day {
+    margin-top: 4px;
+    color: #868e96;
+    font-size: 21px;
+  }
+
+  .tasks-left {
+    color: #20c997;
+    font-size: 18px;
+    margin-top: 40px;
+    font-weight: bold;
+  }
+`;
+
+function TodoHead() {
+  return (
+    <TodoHeadBlock>
+      <h1>2021/08/05</h1>
+      <div className="day">Thursday</div>
+      <div className="tasks-left">Remained 2 works!</div>
+    </TodoHeadBlock>
+  );
+}
+
+export default TodoHead;
